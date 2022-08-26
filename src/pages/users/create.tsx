f2754caed8dcdf7fd7b8ab174456b14f399c0857
wilkgillian <wilkrosa1998@gmail.com>
2022-08-26 14:44:40 -0400

    interface finalizada

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,66 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack
+} from '@chakra-ui/react';
+import { Input } from '../../components/Form/Input';
+import Header from '../../components/Header';
+import Sidebar from '../../components/Sidebar';
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
+        <Sidebar />
+        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+          <Heading size="lg" fontWeight="normal">
+            Criar Usuário
+          </Heading>
+          <Divider my={6} borderColor="gray.700" />
+          <VStack spacing={8}>
+            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+              <Input
+                name="name"
+                type="text"
+                label="Nome Completo"
+                isRequired={true}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                isRequired={true}
+              />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+              <Input
+                name="password"
+                type="password"
+                label="Senha"
+                isRequired={true}
+              />
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirme sua senha"
+                isRequired={true}
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt={8} justify="flex-end">
+            <HStack spacing={4}>
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
 import Header from '../../components/Header';
+import Pagination from '../../components/Pagination';
 import Sidebar from '../../components/Sidebar';
 
 export default function UserList() {
@@ -77,8 +78,63 @@ export default function UserList() {
                   </Flex>
                 </Td>
               </Tr>
+              <Tr>
+                <Td px={6}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Wilk Gillian</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      wilkrosa1998@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Flex>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="small"
+                      colorScheme="teal"
+                      leftIcon={<Icon as={RiEditLine} />}
+                    >
+                      Editar
+                    </Button>
+                  </Flex>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px={6}>
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Wilk Gillian</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      wilkrosa1998@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  <Flex>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="small"
+                      colorScheme="teal"
+                      leftIcon={<Icon as={RiEditLine} />}
+                    >
+                      Editar
+                    </Button>
+                  </Flex>
+                </Td>
+              </Tr>
             </Tbody>
           </Table>
+          <Pagination />
         </Box>
       </Flex>
     </Box>
