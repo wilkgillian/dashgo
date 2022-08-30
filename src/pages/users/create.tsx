9c84c21f38b18c9159973272c6e62f1db6348dbb
wilkgillian <wilkrosa1998@gmail.com>
2022-08-30 14:18:30 -0400

    responsive finished

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -18,13 +18,13 @@ export default function CreateUser() {
       <Header />
       <Flex w="100%" my={6} maxWidth={1480} mx="auto" px={6}>
         <Sidebar />
-        <Box flex={1} borderRadius={8} bg="gray.800" p={8}>
+        <Box flex={1} borderRadius={8} bg="gray.800" p={['6', '8']}>
           <Heading size="lg" fontWeight="normal">
             Criar Usuário
           </Heading>
           <Divider my={6} borderColor="gray.700" />
           <VStack spacing={8}>
-            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+            <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
               <Input
                 name="name"
                 type="text"
@@ -38,7 +38,7 @@ export default function CreateUser() {
                 isRequired={true}
               />
             </SimpleGrid>
-            <SimpleGrid minChildWidth={240} spacing={8} w="100%">
+            <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
               <Input
                 name="password"
                 type="password"
