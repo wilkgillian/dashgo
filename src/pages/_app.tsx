0a14b08cc1c231b3a3a8980fd76acbc9302d6ed6
wilkgillian <wilkrosa1998@gmail.com>
2022-08-31 16:38:31 -0400

    react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 import { makeServer } from '../services/mirage';
 import { theme } from '../styles/theme';
@@ -8,11 +9,15 @@ if (process.env.NODE_ENV === 'development') {
   makeServer();
 }
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
+        <QueryClientProvider client={queryClient}>
         <Component {...pageProps} />
+        </QueryClientProvider>
       </SidebarDrawerProvider>
     </ChakraProvider>
   );
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -15,23 +16,57 @@ import {
   useBreakpointValue
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import { useEffect } from 'react';
+import {
+  JSXElementConstructor,
+  Key,
+  ReactElement,
+  ReactFragment,
+  ReactPortal,
+  useEffect
+} from 'react';
 import { RiAddLine, RiEditLine } from 'react-icons/ri';
+import { useQuery } from 'react-query';
 import Header from '../../components/Header';
 import Pagination from '../../components/Pagination';
 import Sidebar from '../../components/Sidebar';
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('https://localhost:3000/api/users');
+      const data = await response.json();
+
+      const users = data.users.map(
+        (user: {
+          id: number;
+          name: string;
+          email: string;
+          createdAt: string;
+        }) => {
+          return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+              day: '2-digit',
+              month: 'long',
+              year: 'numeric'
+            })
+          };
+        }
+      );
+      return users;
+    },
+    {
+      staleTime: 1000 * 5
+    }
+  );
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   });
-
-  useEffect(() => {
-    fetch('https://localhost:3000/api/users').then(response =>
-      response.json().then(data => console.log(data))
-    );
-  }, []);
 
   return (
     <Box>
@@ -55,117 +90,79 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" w={8}>
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th w={4}>Ações</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Wilk Gillian</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      wilkrosa1998@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Flex>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="teal"
-                    >
-                      {isWideVersion ? (
-                        <Flex justify="space-between">
-                          <Icon as={RiEditLine} /> <Text ml={2}>Editar</Text>
-                        </Flex>
-                      ) : (
-                        <Icon as={RiEditLine} />
-                      )}
-                    </Button>
-                  </Flex>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Wilk Gillian</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      wilkrosa1998@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Flex>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="teal"
-                    >
-                      {isWideVersion ? (
-                        <Flex justify="space-between">
-                          <Icon as={RiEditLine} /> <Text ml={2}>Editar</Text>
-                        </Flex>
-                      ) : (
-                        <Icon as={RiEditLine} />
-                      )}
-                    </Button>
-                  </Flex>
-                </Td>
-              </Tr>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Wilk Gillian</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      wilkrosa1998@gmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                <Td>
-                  <Flex>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="small"
-                      colorScheme="teal"
-                    >
-                      {isWideVersion ? (
-                        <Flex justify="space-between">
-                          <Icon as={RiEditLine} /> <Text ml={2}>Editar</Text>
-                        </Flex>
-                      ) : (
-                        <Icon as={RiEditLine} />
-                      )}
-                    </Button>
-                  </Flex>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              {' '}
+              <Spinner />{' '}
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              {' '}
+              <Text>Falha ao obter dados dos usuários.</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" w={8}>
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th w={4}>Ações</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(
+                    (user: {
+                      id: number;
+                      name: string;
+                      email: string;
+                      createdAt: string;
+                    }) => {
+                      return (
+                        <Tr key={user.id}>
+                          <Td px={['4', '4', '6']}>
+                            <Checkbox colorScheme="pink" />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight="bold">{user.name}</Text>
+                              <Text fontSize="sm" color="gray.300">
+                                {user.email}
+                              </Text>
+                            </Box>
+                          </Td>
+                          {isWideVersion && <Td>{user.createdAt}</Td>}
+                          <Td>
+                            <Flex>
+                              <Button
+                                as="a"
+                                size="sm"
+                                fontSize="small"
+                                colorScheme="teal"
+                              >
+                                {isWideVersion ? (
+                                  <Flex justify="space-between">
+                                    <Icon as={RiEditLine} />{' '}
+                                    <Text ml={2}>Editar</Text>
+                                  </Flex>
+                                ) : (
+                                  <Icon as={RiEditLine} />
+                                )}
+                              </Button>
+                            </Flex>
+                          </Td>
+                        </Tr>
+                      );
+                    }
+                  )}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
